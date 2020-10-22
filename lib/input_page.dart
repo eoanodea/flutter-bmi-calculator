28117e4d2fed77f51881e9ffd72a49c397852646
Eoan O Dea <odeaeoan@gmail.com>
2020-10-22 17:39:05 +0200

    Commented out unused test imports and added font awesome icons

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 const bottomContainerHeight = 80.0;
 const activeCardColour = Color(0xFF1D1E33);
-const bottomContainerColour = Color();
+const bottomContainerColour = Color(0xFFEB1555);
 
 class InputPage extends StatefulWidget {
   @override
@@ -22,7 +23,25 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: [
                 Expanded(
-                  child: ResuableCard(colour: activeCardColour),
+                  child: ResuableCard(
+                    colour: activeCardColour,
+                    cardChild: Column(
+                      children: <Widget>[
+                        Icon(
+                          FontAwesomeIcons.mars,
+                          size: 80.0,
+                        ),
+                        SizedBox(height: 15.0),
+                        Text(
+                          "Male",
+                          style: TextStyle(
+                            fontSize: 18.0,
+                            color: Color(0xFF8D8E98),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
                 ),
                 Expanded(child: ResuableCard(colour: Color(0xFF1D1E33))),
               ],
@@ -48,10 +67,10 @@ class _InputPageState extends State<InputPage> {
             ),
           ),
           Container(
-            color: Color(0xFFEB1555),
+            color: bottomContainerColour,
             margin: EdgeInsets.only(top: 10.0),
             width: double.infinity,
-            height: 80.0,
+            height: bottomContainerHeight,
           )
         ],
       ),
